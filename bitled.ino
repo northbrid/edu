e37f17a443bba1d8933f72c53db3e35f1b5b2a7d
northbrid <82058106+northbrid@users.noreply.github.com>
2026-04-14 20:53:29 +0200

    Ability to kill interrupts and run multiplexing slowly

--- bitled.ino
+++ bitled.ino
@@ -40,3 +40,43 @@ void updateMatrix() {
   }
   matrix.renderBitmap(frame, 8, 12);
 };
+
+uint8_t fb[12] = {0}; 
+
+void updateFB() {
+  memset(fb, 0, 12);
+  for (int y = 0; y < 8; y++)
+    for (int x = 0; x < 12; x++)
+      if (frame[y][x])
+        fb[(y * 12 + x) >> 3] |= (uint8_t)(1u << ((y * 12 + x) & 7));
+}
+
+void mpxStep() {
+  static volatile int i_isr = 0;
+  if ((fb[i_isr >> 3] & (1 << (i_isr % 8))) != 0) {
+    matrix.on(i_isr);
+  } else {
+    matrix.off(i_isr);
+  }
+  i_isr = (i_isr + 1) % 96;
+}
+
+void slowMotion() {
+  volatile int mpxCount = 0;
+  volatile int tmpCount = 0;
+  volatile int delayMul = 1;
+  updateFB();
+  noInterrupts();
+  while(true) {
+    mpxStep();
+    mpxCount++;
+    if (mpxCount >= 3000/delayMul) {
+      delayMul+=1;
+      mpxCount=0;
+    }
+    for(int i=0; i<3000*delayMul; i++) {
+      tmpCount++;
+    }
+  }
+}
+
